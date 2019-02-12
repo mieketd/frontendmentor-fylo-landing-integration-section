14087e799c7599a708fcafa83e9878bbb952d8f1
MieketenDam <mieketd@gmail.com>
2019-02-12 11:43:26 +0100

    added express module & routing + basic markup and styling & fonts

--- project/app.js
+++ project/app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path');
+const router = express.Router();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
 
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+router.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/about.html'));
+});
+
+router.get('/sitemap', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/sitemap.html'));
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
